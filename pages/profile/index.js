fe7cd1dd455535e9fce42b40df82cb86e31ff3a4
ting <ting04180@gmail.com>
2024-01-03 20:13:32 +0800

    改日期

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -5,13 +5,14 @@ const Profile = () => {
   const { isLoggedIn, user, errorMessage } = useSelector((state) => state.user);
 
   if (isLoggedIn) {
+    const date = user.user.date.toISOString().split("T")[0];
     return (
       <>
         <h1 className="text-center">這是你的個人檔案頁面</h1>
         <div>
           <p>你的用戶名稱為: {user.user.username}</p>{" "}
           <p>你的身分為: {user.user.role}</p>
-          <p>註冊日期為: {user.user.date}</p>
+          <p>註冊日期為: {date}</p>
           {/* 其他用戶相關信息 */}
         </div>
       </>
