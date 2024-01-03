9ab1ab20d43d87f4e79dc973b9d59c825ec12676
ting <ting04180@gmail.com>
2024-01-03 20:32:01 +0800

    再次改日期

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -3,8 +3,7 @@ import { useSelector } from "react-redux";
 
 const Profile = () => {
   const { isLoggedIn, user, errorMessage } = useSelector((state) => state.user);
-  const date = user.user.date.toISOString().split("T")[0];
-  console.log(date);
+
   if (isLoggedIn) {
     return (
       <>
@@ -12,7 +11,7 @@ const Profile = () => {
         <div>
           <p>你的用戶名稱為: {user.user.username}</p>{" "}
           <p>你的身分為: {user.user.role}</p>
-          <p>註冊日期為: {date}</p>
+          <p>註冊日期為: {user.user.date.toISOString().split("T")[0]}</p>
           {/* 其他用戶相關信息 */}
         </div>
       </>
